8cef1d0d7cb417aaba97190366cdd1190ee2ff96
Gurveer2711 <singhgurveer337@gmail.com>
2025-06-21 20:57:58 +0530

    Refactor server setup: add middleware for CORS, helmet, rate limiting, and cookie parsing; update route handling

--- server.js
+++ server.js
@@ -1,15 +1,33 @@
 import express from "express";
 import dotenv from "dotenv";
-import connectDB from "./config/db.js";
+import cookieParser from "cookie-parser";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
+import cors from "cors";
+import authRoutes from "./routes/authRoutes.js";
+import videoRoutes from "./routes/videoRoutes.js";
 
 dotenv.config();
-connectDB();
-
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("API Running");
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+
+const limiter = rateLimit({
+  windowMs: 2 * 1000, //2 seconds
+  max: 20, 
+  message: { error: "Too many requests, please try again later." },
+  headers: true,
 });
+app.use(limiter);
+
+//routes
+app.use("/api/auth", authRoutes);
+app.use("/api/videos", videoRoutes);
+
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
